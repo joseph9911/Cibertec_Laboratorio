9e75ac5a94d19b6a31cf2835a30004b0fae87f4c
Luis Enrique <luchoarrieta@hotmail.com>
2016-07-24 13:59:35 -0500

    Clases 24 julio 2016

---
+++ WebDeveloper.Model/Person.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebDeveloper.Model
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+}
---
+++ WebDeveloper.Repository/WebContextDb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.Repository
+{
+    public class WebContextDb : DbContext
+    {
+        public WebContextDb(): base("WebDeveloperConnectionString")
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
