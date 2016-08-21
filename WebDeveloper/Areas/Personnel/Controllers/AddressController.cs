2d4c83cdeafe0bd673502bd5a630e87a1219b19a
juvega <cevegaju@hotmail.com>
2016-08-21 00:21:40 -0500

    Tarea2, first changes

---
+++ WebDeveloper/Areas/Personnel/Controllers/AddressController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.Areas.Personnel.Controllers
+{
+    public class AddressController : PersonBaseController<Address>
+    {
+        // GET: Personnel/Address
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
