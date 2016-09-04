2f4d5bc4ed2b97493dd88744cf33d451e4a61c10
Cesar Velarde <juvega@gmail.com>
2016-09-04 13:51:37 -0500

    migracion a vistas parciales y uso de ajax con JQuery

--- WebDeveloper/Areas/Personnel/Controllers/PersonController.cs
+++ WebDeveloper/Areas/Personnel/Controllers/PersonController.cs
@@ -1,25 +1,39 @@
 ﻿using System;
+using System.Net;
 using System.Web.Mvc;
 using WebDeveloper.Model;
 
 namespace WebDeveloper.Areas.Personnel.Controllers
 {
     public class PersonController : PersonBaseController<Person>
-    {      
+    {
         public ActionResult Index()
         {
-            return View(_repository.PaginatedList((x=>x.ModifiedDate), 1, 15));
+            return View(_repository.PaginatedList((x => x.ModifiedDate), 1, 15));
+        }
+
+        public ActionResult List(int? page, int? size)
+        {
+            if(!page.HasValue || !size.HasValue)
+            {
+                page = 1;
+                size = 15;
+            }
+            return PartialView("_List",_repository.PaginatedList((x => x.ModifiedDate), 
+                page.Value, 
+                size.Value));
         }
 
         public ActionResult Create()
         {
-            return View();
+            return PartialView("_Create");
         }
 
         [HttpPost]
         public ActionResult Create(Person person)
         {
-            if (!ModelState.IsValid) return View(person);
+            if (!ModelState.IsValid)
+                return PartialView("_Create", person);
             person.rowguid = Guid.NewGuid();
             person.ModifiedDate = DateTime.Now;
             person.BusinessEntity = new BusinessEntity
@@ -28,29 +42,29 @@ namespace WebDeveloper.Areas.Personnel.Controllers
                 ModifiedDate = person.ModifiedDate
             };
             _repository.Add(person);
-            return RedirectToAction("Index");
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
 
         public ActionResult Edit(int id)
         {
             var person = _repository.GetById(x => x.BusinessEntityID == id);
             if (person == null) return RedirectToAction("Index");
-            return View(person);
+            return PartialView("_Edit", person);
         }
 
         [HttpPost]
         public ActionResult Edit(Person person)
         {
-            if (!ModelState.IsValid) return View(person);
+            if (!ModelState.IsValid) return PartialView("_Edit", person);
             _repository.Update(person);
-            return RedirectToAction("Index");
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
 
         public ActionResult Delete(int id)
         {
-            var person = _repository.GetById(x=> x.BusinessEntityID==id);
+            var person = _repository.GetById(x => x.BusinessEntityID == id);
             if (person == null) return RedirectToAction("Index");
-            return View(person);
+            return PartialView("_Delete", person);
         }
 
         [HttpPost]
@@ -65,7 +79,7 @@ namespace WebDeveloper.Areas.Personnel.Controllers
         {
             var person = _repository.GetById(x => x.BusinessEntityID == id);
             if (person == null) return RedirectToAction("Index");
-            return View(person);
+            return PartialView("_Details", person);
         }
     }
 }
