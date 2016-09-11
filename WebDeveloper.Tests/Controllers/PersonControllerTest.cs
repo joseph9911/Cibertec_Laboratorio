a5e024a716c68a37d09f76fcde3d73c0b73f54ce
juvega <cevegaju@hotmail.com>
2016-09-11 01:02:54 -0500

    Unit Test Project

---
+++ WebDeveloper.Tests/Controllers/PersonControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WebDeveloper.Areas.Personnel.Controllers;
+using WebDeveloper.Model;
+using Xunit;
+
+namespace WebDeveloper.Tests.Controllers
+{
+    public class PersonControllerTest
+    {
+        private PersonController controller;
+        public PersonControllerTest()
+        {
+            controller = new PersonController();
+        }
+
+        [Fact(DisplayName = "ListActionEmptyParametersTest")]
+        private void ListActionEmptyParametersTest()
+        {
+            var result = controller.List(null, null) as PartialViewResult;
+            result.ViewName.Should().Be("_List");
+
+            var modelCount = (IEnumerable<Person>)result.Model;
+            modelCount.Count().Should().Be(10);
+        }
+    }
+}
---
+++ WebDeveloper.Tests/Repository/BaseRepositoryTest.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+using FluentAssertions;
+using WebDeveloper.Repository;
+using WebDeveloper.Model;
+using System;
+
+namespace WebDeveloper.Tests.Repository
+{
+
+    public class BaseRepositoryTest
+    {
+        private IRepository<Person> _repository;
+        public BaseRepositoryTest()
+        {
+            _repository = new BaseRepository<Person>();
+        }
+        [Fact(DisplayName ="AddTestOk")]
+        public void AddTestOk()
+        {
+            var person = new Person
+            {
+                PersonType = "SC",
+                FirstName = "Test",
+                LastName = "Test",
+                EmailPromotion = 1
+            };
+            person.rowguid = Guid.NewGuid();
+            person.ModifiedDate = DateTime.Now;
+            person.BusinessEntity = new BusinessEntity
+            {
+                ModifiedDate= DateTime.Now,
+                rowguid= person.rowguid
+            };
+
+            var result = _repository.Add(person);
+            result.Should().BeGreaterThan(0);
+        }
+
+        [Fact(DisplayName = "AddTestWrong")]
+        public void AddTestWrong()
+        {
+            var person = new Person();
+            person.rowguid = Guid.NewGuid();
+            try
+            {
+                _repository.Add(person);
+            }
+            catch(Exception ex)
+            {
+                ex.Source.Should().Be("EntityFramework");                
+            }
+        }
+    }
+}
