c599041c06fddfae36635dd47dd5847fd282c029
Juvega <juvega@gmail.com>
2016-09-15 13:24:47 -0500

    Proyecto Inicial

--- WebDeveloper/App_Start/BundleConfig.cs
+++ WebDeveloper/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace WebDeveloper
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css")
-                      .Include("~/Css/site.css"));
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
         }
     }
 }
