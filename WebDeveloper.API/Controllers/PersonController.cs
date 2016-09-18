4b6492738c1d03acd1b32effbc3ba1e17117ad6d
Joseph <joseph5_xd@hotmail.com>
2016-09-18 14:00:25 -0500

    Inicio Web API

---
+++ WebDeveloper.API/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebDeveloper.Model;
+using WebDeveloper.Repository;
+
+namespace WebDeveloper.API.Controllers
+{
+    public class PersonController : ApiController
+    {
+        private IRepository<Person> repository;
+        public PersonController()
+        {
+            repository = new BaseRepository<Person>();
+        }
+
+        [HttpGet]
+        public IHttpActionResult List()
+        {
+            return Ok(repository.PaginatedList(p => p.ModifiedDate, 1, 10));
+        }
+
+    }
+}
